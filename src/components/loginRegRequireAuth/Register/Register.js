cc18b1bd45e26fe71a7626a9af99e4907bb6757f
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-18 01:20:51 +0600

    register form are added

--- src/components/loginRegRequireAuth/Register/Register.js
+++ src/components/loginRegRequireAuth/Register/Register.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Register.css';
 
 const Register = () => {
     return (
-        <div>
-            <h3>Register form</h3>
+        <div className='register-form login-container'>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <form>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+
+                <input type="password" name="password" id="" placeholder='Password' required />
+                <input type="checkbox" name="terms" id="terms" />
+                <label htmlFor="terms">Accept Shawon legal consultant Terms and Conditions</label>
+                <input
+                    className='w-50 mx-auto btn btn-primary mt-2'
+                    type="submit"
+                    value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-primary pe-auto text-decoration-none'>Please Login</Link> </p>
         </div>
     );
 };
