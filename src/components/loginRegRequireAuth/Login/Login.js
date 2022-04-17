8021eb5c892683dc61d12bf9ad3d7e510d1db78f
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-18 02:55:23 +0600

    signInWithAndPassword added in login form

--- src/components/loginRegRequireAuth/Login/Login.js
+++ src/components/loginRegRequireAuth/Login/Login.js
@@ -1,24 +1,65 @@
+import { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../../shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import './Login.css';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    let location = useLocation();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    let from = location.state?.from?.pathname || "/";
+
+    const navigate = useNavigate();
+
+    let errorMessage;
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+    if (error) {
+        errorMessage = <p className='text-danger'>Error: {error.message}</p>
+    }
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email, password)
+    }
 
     return (
         <div className='container mx-auto login-container'>
             <h2 className='text-primary text-center mt-2'>Please Login</h2>
-            <Form>
+            <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
                     Login
                 </Button>
             </Form>
+            {errorMessage}
             <p>New to Shawon legal consultant? <Link to="/register" className='text-primary pe-auto text-decoration-none'>Please Register</Link> </p>
             <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none'>Reset Password</button> </p>
             <SocialLogin></SocialLogin>
