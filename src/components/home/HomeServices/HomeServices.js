0d82fa3f562140005f43cb75b9f6a5d86aded9ca
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-17 19:58:11 +0600

    json file created

---
+++ src/components/home/HomeServices/HomeServices.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeServices = () => {
+    return (
+        <div>
+            <h1>service of law</h1>
+        </div>
+    );
+};
+
+export default HomeServices;
