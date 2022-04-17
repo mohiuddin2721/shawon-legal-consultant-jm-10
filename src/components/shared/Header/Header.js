030cd390814601bba297efcfd27021b0ff626301
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-17 13:57:44 +0600

    logo added in header

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -2,25 +2,28 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo1 from '../../../images/logo (1).png'
 
 const Header = () => {
     return (
         <div>
             <Navbar style={{'backgroundColor' : '#cc8809'}} collapseOnSelect expand="lg" variant="dark" sticky="top">
                 <Container>
-                    <Navbar.Brand as={Link} to="/home">Shawon Legal Consultant</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/home">
+                        <img className='rounded h-100 w-100' src={logo1} alt="" />
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/home">HOME</Nav.Link>
-                            <Nav.Link as={Link} to="/service">SERVICE</Nav.Link>
-                            <Nav.Link as={Link} to="/clientele">CLIENTELE</Nav.Link>
-                            <Nav.Link as={Link} to="/blog">BLOG</Nav.Link>
+                            <Nav.Link className='text-light' as={Link} to="/home">HOME</Nav.Link>
+                            <Nav.Link className='text-light' as={Link} to="/service">SERVICE</Nav.Link>
+                            <Nav.Link className='text-light' as={Link} to="/clientele">CLIENTELE</Nav.Link>
+                            <Nav.Link className='text-light' as={Link} to="/blog">BLOG</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to="/contact">CONTACT</Nav.Link>
-                            <Nav.Link as={Link} to="/aboutMe">ABOUT ME</Nav.Link>
-                            <Nav.Link as={Link} to="/signIn">SIGNIN</Nav.Link>
+                            <Nav.Link className='text-light' as={Link} to="/contact">CONTACT</Nav.Link>
+                            <Nav.Link className='text-light' as={Link} to="/aboutMe">ABOUT ME</Nav.Link>
+                            <Nav.Link className='text-light' as={Link} to="/signIn">SIGNIN</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
