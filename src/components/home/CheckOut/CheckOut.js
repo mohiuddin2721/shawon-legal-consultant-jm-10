9bf6ecb1ac4f77332e4cc8c83d43c4262cb8b67c
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-18 17:23:10 +0600

    check out form complete

--- src/components/home/CheckOut/CheckOut.js
+++ src/components/home/CheckOut/CheckOut.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './CheckOut.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const CheckOut = () => {
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        toast('Thank you for booking');
+        alert('Thank you for your booking!!');
+    }
     return (
-        <div>
-            <h1>This is check out page</h1>
+        <div className='checkout-form m-5'>
+            <h2 style={{ textAlign: 'center' }} className='text-warning m-5'>Please fill checkout Form</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+
+                <input type="number" name="number" id="" placeholder='Phone Number' required />
+
+                <input type="text" name="address" id="" placeholder='Your Address' />
+
+                <input
+                    className='w-25 mx-auto btn btn-warning mt-2'
+                    type="submit"
+                    value="Submit" />
+            </form>
+            <ToastContainer />
         </div>
     );
 };
