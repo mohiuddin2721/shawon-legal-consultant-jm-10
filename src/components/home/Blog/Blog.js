1bd5cf51df5e1eaf14591067e5f8b6426cfc99ef
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-18 14:35:48 +0600

    blog ready

--- src/components/home/Blog/Blog.js
+++ src/components/home/Blog/Blog.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
 import './Blog.css';
 
 const Blog = () => {
     return (
-        <div>
-            <h3>Blog page</h3>
-        </div>
+        <CardGroup className='container'>
+            <Card className='m-4'>
+                <Card.Footer className='border'>
+                    <Card.Title>Difference between authorization and authentication</Card.Title>
+                </Card.Footer>
+                <Card.Body>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='m-4'>
+                <Card.Footer className='border'>
+                    <Card.Title>Difference between authorization and authentication</Card.Title>
+                </Card.Footer>
+                <Card.Body>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='m-4'>
+                <Card.Footer className='border'>
+                    <Card.Title>Difference between authorization and authentication</Card.Title>
+                </Card.Footer>
+                <Card.Body>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </CardGroup>
     );
 };
 
