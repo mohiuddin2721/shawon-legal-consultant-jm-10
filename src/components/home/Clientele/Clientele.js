226cb93d1f751d22ba8da50a1ed10c459c3c64c7
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-18 23:00:14 +0600

    clientele page are updated

--- src/components/home/Clientele/Clientele.js
+++ src/components/home/Clientele/Clientele.js
@@ -1,10 +1,188 @@
 import React from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
 import './Clientele.css';
 
 const Clientele = () => {
     return (
-        <div>
-            <h2>Clientele page</h2>
+        <div className='container'>
+            <h2 className='text-center'>Happy Client</h2>
+            <div className='row'>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>RSPL, India</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>SB Group</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>EXIM Bank Ltd</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Agrani Bank Ltd</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Sonali Bank Ltd</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Inhemeter, China</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Inhemeter, China</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>William E Corner, USA</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>IBSYS Canada</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Mass International Ltd</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>GMAC, USA</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Union Bank Ltd</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Desipro PTE Ltd</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Pugos Bangladesh Ltd</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Alpha Flight Guru</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Legal Expat, Spain</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Steel Eagle, Malta</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Hanzburz, UAE</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Euro Kids India, India</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Gaji groupe</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Bosundhara groupe</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Amanotsha lungiu</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Fress cld</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <CardGroup className='col-md-4 col-6 shadow-sm rounded my-3'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Khan ldt</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </div>
         </div>
     );
 };
